40cde9b8d8261803994e4a1f3398f9779ca0318d
Burak Osman Yaldız <37910667+WinneRose@users.noreply.github.com>
2020-12-05 17:39:11 +0300

    Day 11 of #100daysofjs
    Get Started Vue  , Vue Intro

---
+++ vue.js/week1/intro/src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    
+    <div class="alert alert-primary" role="alert">
+  A simple primary alert—check it out!
+</div>
+  </div>
+
+  
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
